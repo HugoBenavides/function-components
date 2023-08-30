65bdab85364a71147f75b1de419b6d7d66a73d44
Hugo Benavides <the.another.gerf@gmail.com>
2023-08-30 15:26:04 -0600

    Se agrega el componente Container de MUI para poder empezar a hacer el layout de la página con formulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import FormSingUp from './components/FormSingUp';
+import { Container } from '@mui/material';
 
 
 
 function App() {
   return (
-    <>
+    <Container component="section" maxWidth="sm" >
       <h1>Formulario Registro</h1>
       <FormSingUp />
-    </>
+    </Container>
   );
 }
 
